b2dd3334fe2f929feff7db517331fd780586fda9
splat46 <stijnhuiskes@gmail.com>
2020-12-27 10:53:03 +0100

    Added smooth scroll

--- js/main.js
+++ js/main.js
@@ -1,15 +1,36 @@
+// Smooth scroll to next section
+document.querySelectorAll('button a[href^="#"]').forEach((trigger) => {
+  trigger.onclick = function (e) {
+    e.preventDefault();
+    let hash = this.getAttribute("href");
+    let target = document.querySelector(hash);
+    let headerOffset = 100;
+    let elementPosition = target.offsetTop;
+    let offsetPosition = elementPosition - headerOffset;
+
+    window.scrollTo({
+      top: offsetPosition,
+      behavior: "smooth",
+    });
+  };
+});
+
+// Section One Counter + or -
+// default = 0
 let counter = 0;
 
 const buttonCounterMore = document.getElementById("counterButtonMore");
 const buttonCounterLess = document.getElementById("counterButtonLess");
 const counterBoard = document.getElementById("counterBoard");
 
+// increase by 1
 buttonCounterMore.onclick = function () {
   counter = counter + 1;
   counterBoard.innerHTML = counter;
 };
-
+// decrease by 1
 buttonCounterLess.onclick = function () {
+  // prevent lower than 0
   if (counter === 0) {
     counter = 0;
   } else {
